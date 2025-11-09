0c4f201f0ca6413e8e51b804e05cbe1a003f8153
kqf <petrov.knu@gmail.com>
2025-11-09 07:01:34 +0100

    Add the texts also

--- autozeug/yoga.py
+++ autozeug/yoga.py
@@ -25,7 +25,7 @@ class VideoPost:
     video: Path
 
     def __str__(self):
-        return f"{self.date}: {self.video}"
+        return f"{self.date}: {self.video} \n {self.text}"
 
     def valid(self) -> bool:
         return self.video.exists()
